cd162092220b6aec968a8c0197a95124f7a75c6b
Atakan Zengin <atakanzzengin@gmail.com>
2022-11-24 21:27:23 +0100

    Configure minor settings on colly

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -5,12 +5,15 @@ import (
 	"time"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/gocolly/colly/v2/debug"
 )
 
 func NewScraper() {
 	c := colly.NewCollector(
-		// colly.AllowedDomains("google.com"),
+		colly.AllowedDomains("www.google.com", "google.com", "consent.google.com"),
 		colly.AllowURLRevisit(),
+		// colly.UserAgent("firefox"),
+		colly.Debugger(&debug.LogDebugger{}),
 		// colly.Async(true),
 	)
 
@@ -21,19 +24,21 @@ func NewScraper() {
 		RandomDelay: 1 * time.Second,
 	})
 
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Printf("Got response from: %s", r.Request.URL)
+	})
+
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Printf("Something went wrong:\nResponse:\n%s\nError:\n%s", r.Body, err)
 	})
 
-	c.OnHTML("#rso", func(e *colly.HTMLElement) {
-		e.ForEach("a[href]", func(i int, h *colly.HTMLElement) {
-			fmt.Printf("Got this: %s", h.Text)
-		})
+	c.OnHTML("div[id=res]", func(e *colly.HTMLElement) {
+		fmt.Printf("Got: %s", e.Text)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf("Visiting %s\n", r.URL)
 	})
 
-	c.Visit("https://www.google.com/search?site%3Astackoverflow.com+javascript+variables")
+	c.Visit("https://www.google.com/search?q=site%3Astackoverflow.com+react+server+components")
 }
